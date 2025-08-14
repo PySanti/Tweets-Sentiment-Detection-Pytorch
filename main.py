a465bddfc4c968dba92730eb0b6b111977e36349
Santiago <santiagodeandrade4@gmail.com>
2025-08-14 08:45:02 -0400

    Inicio del proyecto

---
+++ main.py
@@ -0,0 +1,8 @@
+import kagglehub
+import pandas as pd
+
+# Download latest version
+path = kagglehub.dataset_download("tariqsays/sentiment-dataset-with-1-million-tweets")
+x = pd.DataFrame(pd.read_csv(path + "/dataset.csv"))
+print(x.shape)
+
