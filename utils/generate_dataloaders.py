af8caec27cb54d6879b2f6189604e570e8d334d5
Santiago <santiagodeandrade4@gmail.com>
2025-08-16 19:57:40 -0400

    Refactor de funcion get_dataloaders

--- utils/generate_dataloaders.py
+++ utils/generate_dataloaders.py
@@ -7,28 +7,15 @@ def sparse_collate_fn(batch):
     X_batch, Y_batch = zip(*batch)
     return torch.stack([x.to_dense() for x in X_batch]), torch.stack(Y_batch)
 
-
-
 def generate_dataloaders(BATCH_SIZE,train_dataset=None, val_dataset=None, test_dataset=None):
-    train_dataloader, validation_dataloader, test_dataloader = None, None, None
-    if train_dataset:
-        train_dataloader = DataLoader(
-                TensorDataset(train_dataset[0], train_dataset[1]), 
-                batch_size=BATCH_SIZE, 
-                shuffle=False, 
-                collate_fn=sparse_collate_fn)
-    if val_dataset:
-        validation_dataloader = DataLoader(
-                TensorDataset(val_dataset[0], val_dataset[1]), 
-                batch_size=BATCH_SIZE, 
-                shuffle=False, 
-                collate_fn=sparse_collate_fn)
-
-    if test_dataset:
-        test_dataloader = DataLoader(
-                TensorDataset(test_dataset[0], test_dataset[1]), 
-                batch_size=BATCH_SIZE, 
-                shuffle=False, 
-                collate_fn=sparse_collate_fn)
-    return train_dataloader, validation_dataloader, test_dataloader
-
+    dataloaders = []
+    for dataset in [train_dataset, val_dataset, test_dataset]:
+        new_dataloder = None
+        if dataset:
+            new_dataloder = DataLoader(
+                    TensorDataset(dataset[0], dataset[1]), 
+                    batch_size=BATCH_SIZE, 
+                    shuffle=False, 
+                    collate_fn=sparse_collate_fn)
+        dataloaders.append(new_dataloder)
+    return dataloaders
